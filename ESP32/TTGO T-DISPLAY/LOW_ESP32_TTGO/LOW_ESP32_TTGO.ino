2c831b388c97f0bd8d67a9b5b8887df535371ef9
OtavioAuRamos <otaviosk9@gmail.com>
2024-08-23 00:12:09 -0300

    Reworkt at single endpoint, use multiples generate refresh errors at TTGO

--- ESP32/TTGO T-DISPLAY/LOW_ESP32_TTGO/LOW_ESP32_TTGO.ino
+++ ESP32/TTGO T-DISPLAY/LOW_ESP32_TTGO/LOW_ESP32_TTGO.ino
@@ -3,24 +3,19 @@
 #include <HTTPClient.h>
 #include <ArduinoJson.h>
 
-const char* ssid = ""; // Enter SSID
-const char* password = ""; // Enter PASSWORD
-const int* refreshRate = 500
+const char* ssid = "";     // Enter SSID
+const char* password = "";  // Enter PASSWORD
+const int refreshRate = 500;
 
 // ExpressJS - Url
-// TODO - Enter serverCpu/Gpu/Ram/Disk
-const char* serverCpu = "http://localhost:5000/cpu"; // Change "localhost" for IP if you can use staticIp!
-const char* serverGpu = "http://localhost:5000/gpu";
-const char* serverRam = "http://localhost:5000/ram";
-const char* serverDisk = "http://localhost:5000/disk";
-
+const char* expressJs = "http://localhost:5000/monitor"; // Change "localhost" for IP if you can use staticIp!
 
 // TODO - Add later how to enable TFT_eSPI()
 TFT_eSPI tft = TFT_eSPI();  // Invoke library, pins defined in User_Setup.h
 
 void setup() {
   Serial.begin(115200);
-  
+
   // Initialize the display
   tft.init();
   tft.setRotation(1);
@@ -31,63 +26,115 @@ void setup() {
   // Connect to WiFi
   WiFi.begin(ssid, password);
   tft.setCursor(0, 0);
-  tft.println("Connecting to WiFi...");
-  
+  tft.println("Wifi Status [...]");
+
   while (WiFi.status() != WL_CONNECTED) {
-    delay(5000);
-    Serial.println("Connecting to WiFi...");
+    delay(3000);
+    Serial.println("Wifi Status [FAILED]");
   }
 
   // Connected sucessefuly
   tft.fillScreen(TFT_BLACK);
-  tft.println("Success! Wifi Conected");
+  tft.setCursor(0, 0);
+  tft.println("Wifi Status [OK]");
+
   delay(1000);
 }
 
 void loop() {
-  if ((WiFi.status() == WL_CONNECTED)) { // Check the current connection status
+  if ((WiFi.status() == WL_CONNECTED)) {  // Check the current connection status
 
     HTTPClient http;
+    http.begin(expressJs);  // Start CPU endpoint
+    int httpResponseCode = http.GET();
 
-    http.begin(serverCpu); // Specify the URL
-    int httpCode = http.GET(); // Make the request
+    if (httpResponseCode > 0) {
 
-    if (httpCode > 0) { // Check for the returning code
-
-      String payload = http.getString(); // Get the request response payload
+      String payload = http.getString();
       Serial.println(payload);
 
-      // Parse JSON data
-      DynamicJsonDocument doc(200);
-      deserializeJson(doc, payload);
+      // Parsing JSON
+      StaticJsonDocument<1024> doc;
+      DeserializationError error = deserializeJson(doc, payload);
 
-      // Extract data (example: CPU temperature)
-      const char* cpuValue = doc["value"];
+      if (!error) {
+        const char* cpu_id = doc["cpu"]["id"];
+        const char* cpu_load = doc["cpu"]["load"];
+        const char* cpu_temp = doc["cpu"]["temp"];
 
-      // Clear the screen
-      tft.fillScreen(TFT_BLACK);
+        const char* gpu_id = doc["gpu"]["id"];
+        const char* gpu_load = doc["gpu"]["load"];
+        const char* gpu_temp = doc["gpu"]["temp"];
 
-      // Display CPU - Usage
-      tft.setCursor(0, 0);
-      tft.setTextColor(TFT_GREEN);
-      tft.setTextSize(2);
-      tft.print("CPU Load: ");
-      tft.print(cpuValue);
+        const char* ram_id = doc["ram"]["id"];
+        const char* ram_load = doc["ram"]["load"];
+        const char* ram_max = doc["ram"]["max"];
 
-      // Display GPU - Usage
-      // tft.setCursor(0, 30);
-      // tft.setTextColor(TFT_GREEN);
-      // tft.setTextSize(2);
-      // tft.print("GPULoad: ");
-      // tft.print("N/A");
+        const char* disk_id = doc["disk"]["id"];
+        const char* disk_load = doc["disk"]["load"];
+        const char* disk_temp = doc["disk"]["temp"];
+
+        // Display data
+        tft.fillScreen(TFT_BLACK);  // Clear screen
+        tft.setCursor(0, 0);
+        tft.setTextSize(2);
+
+
+        // Display CPU
+        tft.setTextColor(TFT_GREEN);
+        tft.print("CPU:");
+        tft.setCursor(0, tft.getCursorY() + tft.fontHeight());
+
+        tft.setTextColor(TFT_DARKGREEN);
+        tft.print(cpu_load);
+        tft.print(" | ");
+        tft.print(cpu_temp);
+        tft.setCursor(0, tft.getCursorY() + tft.fontHeight());
+
+
+        // Display GPU
+        tft.setTextColor(TFT_GREEN);
+        tft.print("GPU:");
+        tft.setCursor(0, tft.getCursorY() + tft.fontHeight());
+
+        tft.setTextColor(TFT_DARKGREEN);
+        tft.print(gpu_load);
+        tft.print(" | ");
+        tft.print(gpu_temp);
+        tft.setCursor(0, tft.getCursorY() + tft.fontHeight());
+
+
+        // Display RAM
+        tft.setTextColor(TFT_GREEN);
+        tft.print("RAM:");
+        tft.setCursor(0, tft.getCursorY() + tft.fontHeight());
+
+        tft.setTextColor(TFT_DARKGREEN);
+        tft.print(ram_load);
+        tft.print(" | ");
+        tft.print(ram_max);
+        tft.setCursor(0, tft.getCursorY() + tft.fontHeight());
+
+
+        // Display Disk
+        tft.setTextColor(TFT_GREEN);
+        tft.print("DISK:");
+        tft.setCursor(0, tft.getCursorY() + tft.fontHeight());
+
+        tft.setTextColor(TFT_DARKGREEN);
+        tft.print(disk_load);
+        tft.print(" | ");
+        tft.print(disk_temp);
+
+      } else {
+        Serial.println("Failed to parse JSON");
+      }
+    } else {
+      Serial.printf("Error code: %d\n", httpResponseCode);
     }
-
-    else {
-      Serial.println("Error on HTTP request");
-    }
-
-    http.end(); // Free the resources
+    http.end();
   }
-
-  delay(refreshRate); // {refreshRate} in seconds
+  delay(1000);  // {refreshRate} in seconds
 }
+
+// Say thanks to Obesity King aka. Otavio A. Ramos
--- src/server.js
+++ src/server.js
@@ -9,115 +9,48 @@ const { findById } = require('./findById')
 // LibreOpenHardware - Webserver Url
 const url = process.env.LOW_URL || 'http://localhost:8085/data.json'
 
-app.get('/cpu', (req, res) => {
+app.get('/monitor', (req, res) => {
   res.setHeader('Content-Type', 'text/event-stream')
   res.setHeader('Cache-Control', 'no-cache')
   res.setHeader('Connection', 'keep-alive')
 
-  const intervalId = setInterval(async () => {
-    try {
-      const response = await axios.get(url)
-      const findCpuName = findById(response.data, 43)
-      const findCpuLoad = findById(response.data, 81)
-      const findCpuTemp = findById(response.data, 79)
-
-      const result = {
-        id: findCpuName.Text,
-        load: findCpuLoad.Value,
-        temp: findCpuTemp.Value
-      }
-      return res.json(result)
-    } catch (error) {
-      return res.status(500).send('Error fetching hardware data')
-    }
-  }, 1000)
-
-  req.on('close', () => {
-    clearInterval(intervalId)
-    res.end()
-  })
-})
-
-app.get('/gpu', (req, res) => {
-  res.setHeader('Content-Type', 'text/event-stream')
-  res.setHeader('Cache-Control', 'no-cache')
-  res.setHeader('Connection', 'keep-alive')
+  const getDataById = (data, id) => findById(data, id)
 
   const intervalId = setInterval(async () => {
     try {
       const response = await axios.get(url)
-      const findGpuName = findById(response.data, 104)
-      const findGpuLoad = findById(response.data, 116)
-      const findGpuTemp = findById(response.data, 113)
+      const data = response.data
+
+      const hardwareInfo = (nameId, loadId, tempOrMaxId) => ({
+        id: getDataById(data, nameId).Text,
+        load: getDataById(data, loadId).Value,
+        tempOrMax: getDataById(data, tempOrMaxId).Value
+      })
 
       const result = {
-        id: findGpuName.Text,
-        load: findGpuLoad.Value,
-        temp: findGpuTemp.Value
+        cpu: {
+          ...hardwareInfo(43, 81, 79),
+          temp: getDataById(data, 79).Value
+        },
+        gpu: {
+          ...hardwareInfo(104, 116, 113),
+          temp: getDataById(data, 113).Value
+        },
+        ram: {
+          ...hardwareInfo(95, 97, 102),
+          max: getDataById(data, 102).Value
+        },
+        disk: {
+          ...hardwareInfo(137, 144, 139),
+          temp: getDataById(data, 139).Value
+        }
       }
+
       return res.json(result)
     } catch (error) {
       return res.status(500).send('Error fetching hardware data')
     }
-  }, 1000)
-
-  req.on('close', () => {
-    clearInterval(intervalId)
-    res.end()
-  })
-})
-
-app.get('/ram', (req, res) => {
-  res.setHeader('Content-Type', 'text/event-stream')
-  res.setHeader('Cache-Control', 'no-cache')
-  res.setHeader('Connection', 'keep-alive')
-
-  const intervalId = setInterval(async () => {
-    try {
-      const response = await axios.get(url)
-      const findRamName = findById(response.data, 95)
-      const findRamLoad = findById(response.data, 97)
-      const findRamMax = findById(response.data, 102)
-
-      const result = {
-        id: findRamName.Text,
-        load: findRamLoad.Value,
-        max: findRamMax.Value
-      }
-      return res.json(result)
-    } catch (error) {
-      return res.status(500).send('Error fetching hardware data')
-    }
-  }, 1000)
-
-  req.on('close', () => {
-    clearInterval(intervalId)
-    res.end()
-  })
-})
-
-app.get('/disk', (req, res) => {
-  res.setHeader('Content-Type', 'text/event-stream')
-  res.setHeader('Cache-Control', 'no-cache')
-  res.setHeader('Connection', 'keep-alive')
-
-  const intervalId = setInterval(async () => {
-    try {
-      const response = await axios.get(url)
-      const findDiskName = findById(response.data, 137)
-      const findDiskLoad = findById(response.data, 144)
-      const findDiskTemp = findById(response.data, 139)
-
-      const result = {
-        id: findDiskName.Text,
-        load: findDiskLoad.Value,
-        temp: findDiskTemp.Value
-      }
-      return res.json(result)
-    } catch (error) {
-      return res.status(500).send('Error fetching hardware data')
-    }
-  }, 1000)
+  }, 100)
 
   req.on('close', () => {
     clearInterval(intervalId)
